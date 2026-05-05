a9d522a8335ece7a694f5e223716b97559d518f8
VlexandreMG <dodolashmou@gmail.com>
2026-05-05 14:58:26 +0300

    3nd commit

--- src/main/java/com/example/demo/models/Demande.java
+++ src/main/java/com/example/demo/models/Demande.java
@@ -1,5 +1,57 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "demande")
 public class Demande {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToMany
+    @JoinColumn(name = "id_client")
+    private int idClient;
+
+    @OneToMany
+    @JoinColumn(name = "id_commune")
+    private int idCommune;
+
+    @Column(name = "lieu")
+    private String lieu;
+
+    public Demande() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public int getIdCommune() {
+        return idCommune;
+    }
+
+    public void setIdCommune(int idCommune) {
+        this.idCommune = idCommune;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
 }
