7dedf744fa0ce53af0d9202b03e3d618369198ad
VlexandreMG <dodolashmou@gmail.com>
2026-05-15 11:08:20 +0300

    Zay vô nivoaka le izy oeeee XD

--- src/main/java/com/example/demo/controllers/DemandeController.java
+++ src/main/java/com/example/demo/controllers/DemandeController.java
@@ -7,15 +7,17 @@ import com.example.demo.models.Demande;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.demo.services.DemandeService;
 
 @Controller
+@RequestMapping("/demandes")
 public class DemandeController {
 
     @Autowired
     private DemandeService demandeService;
     
-    @GetMapping("/demandes/new")
+    @GetMapping("/new")
     public String afficherFormulaire(Model model) {
         model.addAttribute("demande", new Demande());
         return "demandes/formulaire";
@@ -24,6 +26,6 @@ public class DemandeController {
     @PostMapping("/save")
     public String enregistrerDemande(@ModelAttribute("demande") Demande demande) {
         demandeService.saveNewDemande(demande);
-        return "redirect:/demandes/save";
+        return "redirect:/demandes/new";
     }
 }
