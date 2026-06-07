5ca54daaa2d5071ef481a8f5da71780fb9f62b53
VlexandreMG <dodolashmou@gmail.com>
2026-06-07 18:31:24 +0300

    Modèle devis

---
+++ src/main/java/com/example/demo/models/Devis.java
@@ -0,0 +1,68 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "devis")
+public class Devis {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @Column(name="demande_id")
+    private Demande demande;
+
+    @ManyToOne 
+    @Column(name="devis_type")
+    private DevisType devisType;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "observation")
+    private String observation;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Demande getDemande() {
+        return demande;
+    }
+
+    public void setDemande(Demande demande) {
+        this.demande = demande;
+    }
+
+    public DevisType getDevisType() {
+        return devisType;
+    }
+
+    public void setDevisType(DevisType devisType) {
+        this.devisType = devisType;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+}
